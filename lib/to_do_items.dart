a7b1c49e92e4b88c17a7d20707cd67f8cf4449af
Ryan Fuller <fullerra@hendrix.edu>
2022-09-06 20:22:16 -0500

    Circle Avatar with abbreviation
    Passed the "ToDoListItem has a Circle Avatar with abbreviation" test.

--- lib/to_do_items.dart
+++ lib/to_do_items.dart
@@ -33,7 +33,7 @@ class ToDoListItem extends StatelessWidget {
     // taking place and therefore which theme to use.
 
     return completed //
-        ? Colors.black
+        ? Colors.black54
         : Theme.of(context).primaryColor;
   }
 
